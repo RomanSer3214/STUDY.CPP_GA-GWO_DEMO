1ddc653c9f083acc7e2d8beab72ba407647e4bde
SRoman <SRoman>
2025-10-24 19:30:36 +0300

    Added cmake

---
+++ src/build/CMakeFiles/Demo_App.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/roman/programming/C++/UNI PROJECTS/P1/STUDY.CPP_GA-GWO_DEMO/src/DrawScene.cpp" "CMakeFiles/Demo_App.dir/DrawScene.cpp.o" "gcc" "CMakeFiles/Demo_App.dir/DrawScene.cpp.o.d"
+  "/home/roman/programming/C++/UNI PROJECTS/P1/STUDY.CPP_GA-GWO_DEMO/src/GA.cpp" "CMakeFiles/Demo_App.dir/GA.cpp.o" "gcc" "CMakeFiles/Demo_App.dir/GA.cpp.o.d"
+  "/home/roman/programming/C++/UNI PROJECTS/P1/STUDY.CPP_GA-GWO_DEMO/src/GWO.cpp" "CMakeFiles/Demo_App.dir/GWO.cpp.o" "gcc" "CMakeFiles/Demo_App.dir/GWO.cpp.o.d"
+  "/home/roman/programming/C++/UNI PROJECTS/P1/STUDY.CPP_GA-GWO_DEMO/backends/imgui_impl_glfw.cpp" "CMakeFiles/Demo_App.dir/home/roman/programming/C++/UNI_PROJECTS/P1/STUDY.CPP_GA-GWO_DEMO/backends/imgui_impl_glfw.cpp.o" "gcc" "CMakeFiles/Demo_App.dir/home/roman/programming/C++/UNI_PROJECTS/P1/STUDY.CPP_GA-GWO_DEMO/backends/imgui_impl_glfw.cpp.o.d"
+  "/home/roman/programming/C++/UNI PROJECTS/P1/STUDY.CPP_GA-GWO_DEMO/backends/imgui_impl_opengl3.cpp" "CMakeFiles/Demo_App.dir/home/roman/programming/C++/UNI_PROJECTS/P1/STUDY.CPP_GA-GWO_DEMO/backends/imgui_impl_opengl3.cpp.o" "gcc" "CMakeFiles/Demo_App.dir/home/roman/programming/C++/UNI_PROJECTS/P1/STUDY.CPP_GA-GWO_DEMO/backends/imgui_impl_opengl3.cpp.o.d"
+  "/home/roman/programming/C++/UNI PROJECTS/P1/STUDY.CPP_GA-GWO_DEMO/imgui/imgui.cpp" "CMakeFiles/Demo_App.dir/home/roman/programming/C++/UNI_PROJECTS/P1/STUDY.CPP_GA-GWO_DEMO/imgui/imgui.cpp.o" "gcc" "CMakeFiles/Demo_App.dir/home/roman/programming/C++/UNI_PROJECTS/P1/STUDY.CPP_GA-GWO_DEMO/imgui/imgui.cpp.o.d"
+  "/home/roman/programming/C++/UNI PROJECTS/P1/STUDY.CPP_GA-GWO_DEMO/imgui/imgui_demo.cpp" "CMakeFiles/Demo_App.dir/home/roman/programming/C++/UNI_PROJECTS/P1/STUDY.CPP_GA-GWO_DEMO/imgui/imgui_demo.cpp.o" "gcc" "CMakeFiles/Demo_App.dir/home/roman/programming/C++/UNI_PROJECTS/P1/STUDY.CPP_GA-GWO_DEMO/imgui/imgui_demo.cpp.o.d"
+  "/home/roman/programming/C++/UNI PROJECTS/P1/STUDY.CPP_GA-GWO_DEMO/imgui/imgui_draw.cpp" "CMakeFiles/Demo_App.dir/home/roman/programming/C++/UNI_PROJECTS/P1/STUDY.CPP_GA-GWO_DEMO/imgui/imgui_draw.cpp.o" "gcc" "CMakeFiles/Demo_App.dir/home/roman/programming/C++/UNI_PROJECTS/P1/STUDY.CPP_GA-GWO_DEMO/imgui/imgui_draw.cpp.o.d"
+  "/home/roman/programming/C++/UNI PROJECTS/P1/STUDY.CPP_GA-GWO_DEMO/imgui/imgui_tables.cpp" "CMakeFiles/Demo_App.dir/home/roman/programming/C++/UNI_PROJECTS/P1/STUDY.CPP_GA-GWO_DEMO/imgui/imgui_tables.cpp.o" "gcc" "CMakeFiles/Demo_App.dir/home/roman/programming/C++/UNI_PROJECTS/P1/STUDY.CPP_GA-GWO_DEMO/imgui/imgui_tables.cpp.o.d"
+  "/home/roman/programming/C++/UNI PROJECTS/P1/STUDY.CPP_GA-GWO_DEMO/imgui/imgui_widgets.cpp" "CMakeFiles/Demo_App.dir/home/roman/programming/C++/UNI_PROJECTS/P1/STUDY.CPP_GA-GWO_DEMO/imgui/imgui_widgets.cpp.o" "gcc" "CMakeFiles/Demo_App.dir/home/roman/programming/C++/UNI_PROJECTS/P1/STUDY.CPP_GA-GWO_DEMO/imgui/imgui_widgets.cpp.o.d"
+  "/home/roman/programming/C++/UNI PROJECTS/P1/STUDY.CPP_GA-GWO_DEMO/src/main.cpp" "CMakeFiles/Demo_App.dir/main.cpp.o" "gcc" "CMakeFiles/Demo_App.dir/main.cpp.o.d"
+  "" "Demo_App" "gcc" "CMakeFiles/Demo_App.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
